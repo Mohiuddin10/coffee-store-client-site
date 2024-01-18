101b44de24c60d0ae5a146915baefbf828690ca0
Mohiuddin10 <sj.ctg6737@gmail.com>
2024-01-18 21:03:58 +0600

    error in createUser

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1RGZt__RiGGFTG4sgLKcjiF_7BGmDgDk",
+  authDomain: "coffee-store-54bf5.firebaseapp.com",
+  projectId: "coffee-store-54bf5",
+  storageBucket: "coffee-store-54bf5.appspot.com",
+  messagingSenderId: "809694926251",
+  appId: "1:809694926251:web:d9c024e3dbe8084b582eee"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
